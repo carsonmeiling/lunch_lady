20910467f6fec2d863ed28d8edeb9adeba43d87f
Carson Meiling <carmc93@gmail.com>
2020-02-19 20:16:10 -0700

    push

---
+++ lunch_lady.rb
@@ -0,0 +1,55 @@
+class MainDish
+  attr_accessor :item, :price
+
+  def initialize(item, price)
+    @item = item
+    @price = price
+  end
+  def make_list
+    @main_list = []
+    new_item = {}
+    new_item["item".to_sym] = @item
+    new_item["price".to_sym] = @price
+    @main_list << new_item
+    puts @main_list
+  end
+end
+
+class SideDish
+  attr_accessor :item, :price
+  
+  def initialize(item, price)
+    @item = item
+    @price = price
+  end
+
+  def make_list
+    @side_list = []
+    new_item = {}
+    new_item["item".to_sym] = @item
+    new_item["price".to_sym] = @price
+    @side_list << new_item
+    puts @side_list
+  end
+end
+
+
+class LunchLady
+  def initialize(name)
+    @name = name
+    @main_dish = []
+    @side_dish = []
+  end
+
+  def show_main_menu
+    MainDish.new('noodles', 3.50)
+    carrots.make_list
+    MainDish.new('pizza', 4.50)
+    pizza.make_list
+  end
+
+end
+
+carson = LunchLady.new("carson")
+
+carson.show_main_menu
